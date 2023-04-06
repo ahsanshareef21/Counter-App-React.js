e96ce95a995226a8d2f28d13c3184455bc2df204
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-06 15:28:32 +0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [inputValue, setinputValue] = useState(0);
+
+  // const btnStyle = {
+  //   padding: 20,
+  //   border: "none",
+  //   backgroundColor: "red",
+  //   border: "5px solid white",
+  //   // padding: "5px",
+  //   margin: 10,
+  // };
+
+  const increment = () => {
+    setinputValue(inputValue + 1);
+  };
+
+  const decrement = () => {
+    setinputValue(inputValue - 1);
+  };
+
+  useEffect(() => {
+    console.log("useEffect Hook");
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div>
+      <input
+        style={{
+          padding: "20px",
+          border: "2px solid blue",
+        }}
+        type="number"
+        placeholder=" Enter Somthing"
+        alt="Form "
+        value={inputValue}
+      />
 
-export default App
+      <button style={btnStyle} onClick={increment}>
+        <h1>+</h1>{" "}
+      </button>
+      <button style={btnStyle} onClick={decrement}>
+        {" "}
+        <h1>-</h1>{" "}
+      </button>
+    </div>
+  );
+};
+
+export default App;

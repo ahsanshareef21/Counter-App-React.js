6a45c10f98b7b149c890054f8b755143ad00c259
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2023-04-07 09:49:00 +0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
   const [inputValue, setinputValue] = useState(0);
-
-  // const btnStyle = {
-  //   padding: 20,
-  //   border: "none",
-  //   backgroundColor: "red",
-  //   border: "5px solid white",
-  //   // padding: "5px",
-  //   margin: 10,
-  // };
 
   const increment = () => {
     setinputValue(inputValue + 1);
@@ -22,30 +13,28 @@ const App = () => {
     setinputValue(inputValue - 1);
   };
 
-  useEffect(() => {
-    console.log("useEffect Hook");
-  }, []);
-
   return (
-    <div>
+    <div className=" main1">
+      <div>
+        <h2 className="main2"> Counter App Using use State Hook in React Js</h2>
+      </div>
+
       <input
-        style={{
-          padding: "20px",
-          border: "2px solid blue",
-        }}
+        className="input"
         type="number"
-        placeholder=" Enter Somthing"
-        alt="Form "
         value={inputValue}
       />
+      <br />
+      <br />
+      <div>
+        <button onClick={increment}>
+          <h1 className="btn"> + </h1>
+        </button>
 
-      <button style={btnStyle} onClick={increment}>
-        <h1>+</h1>{" "}
-      </button>
-      <button style={btnStyle} onClick={decrement}>
-        {" "}
-        <h1>-</h1>{" "}
-      </button>
+        <button onClick={decrement}>
+          <h1 className="btn">-</h1>
+        </button>
+      </div>
     </div>
   );
 };
